404228704d7d3fd4035058b65b0b71cb57dd9da4
waltersantos-muniz <walter.ms.santos41@gmail.com>
2019-08-15 11:42:42 -0300

    interface java

--- Biblioteca/src/biblioteca/Principal.java
+++ Biblioteca/src/biblioteca/Principal.java
@@ -1,21 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package biblioteca;
 
-/**
- *
- * @author Usuário
- */
+
 public class Principal {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+   Interfacxe i = new Interfacxe();
+   i.show();
     }
     
 }
---
+++ Biblioteca/src/biblioteca/livro.java
@@ -0,0 +1,89 @@
+package biblioteca;
+
+public class livro {
+   private String titulo;
+   private int ano;
+   private int iespn;
+   private double  qtdPaginas;
+   private String editora;
+   private  double valor;
+    
+    public String getTitulo(String titulo){
+       return this.titulo();
+    }
+    public void setTitulo(){   
+        this.titulo = titulo;
+    }
+    
+    public int getAno(int ano){
+        return this.ano();
+    }
+    public void setAno(){ 
+        this.ano = ano;
+    }
+    
+    public int getIESPN(int iespn){
+        return this.iespn();
+    }
+    public void setIESPN(){
+        this.iespn = iespn;
+    }
+    
+    public double getQTDpaginas(double qtdpaginas){
+        return this.qtdPaginas();
+    }   
+    public void setQTDpaginas(){
+       this.qtdPaginas = qtdPaginas;
+    } 
+    
+    public String getEditora(String editora){
+        return this.editora();
+    }
+    public void setEditora(){
+        this.editora = editora;
+    }
+    
+    public double getValor(double valor){
+        return this.valor();
+    }
+    public void setValor(){
+        this.valor = valor;
+    }
+    
+    public String exibir(){
+        System.out.println("Titulo:");
+        System.out.println("ANO:");
+        System.out.println("IESPN:");
+        System.out.println("QTDpaginas:");
+        System.out.println("Editora:");
+        System.out.println("Valor:");
+        return this.exibir();
+        
+    }
+    
+    
+
+    private String editora() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private String titulo() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private int ano() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private int iespn() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private double qtdPaginas() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private double valor() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
